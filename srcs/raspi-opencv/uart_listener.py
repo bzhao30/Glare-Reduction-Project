af6e4eaa3a5f054efa8ecffbde1c1c954dafcf1f
brad <bzhao30@gmail.com>
2024-06-23 23:25:07 +0100

    update

--- srcs/raspi-opencv/uart_listener.py
+++ srcs/raspi-opencv/uart_listener.py
@@ -1,12 +1,18 @@
 import serial
 import subprocess
+import logging
 
-ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
+logging.basicConfig(filename='/home/brad/uart_listener.log', level=logging.INFO)
+logging.info('UART Listener started')
+
+ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
 
 while True:
     command = ser.readline().decode('utf-8').strip()
     if command:
+        logging.info(f'Received command: {command}')
         try:
             subprocess.run(command, shell=True)
         except Exception as e:
             ser.write(f"Error: {str(e)}\n".encode('utf-8'))
+            logging.error(f'Error executing command: {str(e)}')
